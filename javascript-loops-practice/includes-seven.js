6f029ccf2fdcbb8babad4a29e1f89f94a990587b
Hannah Engelhardt <hannahnengel@gmail.com>
2021-10-28 18:53:16 -0700

    Completed javascript-loops-practice

---
+++ javascript-loops-practice/includes-seven.js
@@ -0,0 +1,12 @@
+/* exported includesSeven */
+
+function includesSeven(array) {
+  var containsSeven = false;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === 7) {
+      containsSeven = true;
+      break;
+    }
+  }
+  return containsSeven;
+}
